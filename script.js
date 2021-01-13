8ce6f9a99b2f0ca60ef5d7f320cd3609dd4f301f
Ben Hong <benhong654@gmail.com>
2021-01-13 15:23:43 -0800

    images and text copies into gallery

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+window.addEventListener('load', function () {
 
-function memeMaker() {
+    let submitter = document.getElementById("submitter");
+    submitter.addEventListener('click', function() {
+        let gallery = document.getElementById("gallery");
+        let div = document.createElement("div");
+        div.className = "image";
+        let img = document.createElement("img");
+        let imgSource = document.getElementsByClassName("meme-image")[0].src;
+        img.src = imgSource;
+        img.className = 'meme-image';
+        div.append(img);
+        let topText = document.createElement("h2");
+        topText.className = 'top-text';
+        div.append(topText);
+        let topVal = document.getElementById("meme-visual").getElementsByClassName("top-text")[0].textContent;
+        topText.innerHTML = topVal;
+        let botText = document.createElement("h2");
+        botText.className = 'bot-text';
+        div.append(botText);
+        let botVal = document.getElementById("meme-visual").getElementsByClassName("bot-text")[0].textContent;
+        botText.innerHTML = botVal;
+        gallery.append(div);
+        let inputs = document.getElementsByClassName("meme-inputs")[0].getElementsByTagName("input");
+        for (let i = 0; i < inputs.length; i++) {
+            document.getElementById(inputs[i].id).value = "";
+        }
+    })
+  })
+  function memeMaker() {
     let imageURL = document.getElementById("imageurl").value;
-    let alt = imageURL === document.getElementById("meme-image").alt;
+    let alt = imageURL === document.getElementsByClassName("meme-image")[0].alt;
     if (imageURL === '' || imageURL === alt) {
         return false;   
     } 
     let topTextVal = document.getElementById("toptext").value;
     let botTextVal = document.getElementById("bottomtext").value;
-    document.getElementById("meme-image").src = imageURL;
+    document.getElementsByClassName("meme-image")[0].src = imageURL;
     let topTextMeme = document.getElementById("meme-visual").getElementsByClassName("top-text")[0];
     let botTextMeme = document.getElementById("meme-visual").getElementsByClassName("bot-text")[0];
     topTextMeme.innerHTML = topTextVal;
     botTextMeme.innerHTML = botTextVal;
 }
 
-// on submit function
-// create a div with image class, 2 h2 divs, with classes top and bot text, and the innerHTML text in it
-function memeSubmitter() {
-    
-}
